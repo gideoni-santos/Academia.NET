1af1e301505e77459fa2735382166b0ad7f450f8
Gideoni Santos <115567537+gideoni-santos@users.noreply.github.com>
2023-10-04 15:04:22 -0300

    Questão 4 respondida

--- exerciciosSequenciais/Program.cs
+++ exerciciosSequenciais/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace exerciciosSequenciais
+﻿using System.Net;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System;
+using Microsoft.VisualBasic;
+
+namespace exerciciosSequenciais
 {
     internal class Program
     {
@@ -78,12 +86,64 @@
 
             // 4) Faça um programa em C# e no Visual Studio que receba a data na forma DDMMAAAA e imprima na forma AAAAMMDD e AAMMDD.Note que o dia, o mês e o ano devem ser lidos em variáveis diferentes.
 
+            DateTime dataNow = DateTime.Now;
+            Console.WriteLine("A data de hoje é: " + dataNow);
+
+            int dia = dataNow.Day;
+            int mes = dataNow.Month;
+            int ano = dataNow.Year;
+            int ano2digitos = dataNow.Year % 100;
+
+            string formato1 = ano.ToString("D4") + "/" + mes.ToString("D2") + "/" + dia.ToString("D2");
+            string formato2 = ano2digitos.ToString("D2") + "/" + mes.ToString("D2") + "/" + dia.ToString("D2");
+
+            Console.WriteLine("A data atual no formato AAAA/MM/DD é: " + formato1);
+
+            Console.WriteLine("A data atual no formato AA/MM/DD é: " + formato2);
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+            //--------------------------------------------------------------
+
+            // 9) Uma loja vende seus produtos no sistema de entrada mais duas prestações, sendo a entrada maiorou igual às prestações, que devem ser iguais, inteiras e as maiores possíveis.
+            //Por exemplo, se o valor da mercadoria for R$ 270, 00, a entrada e as duas prestações são iguais a R$ 90, 00;
+            //se o valor da mercadoria for R$ 302, 75 a entrada é de R$ 102, 75 e as duas prestações são iguais a R$ 100, 00.
+            //Escreva um programa em C# e no Visual Studio que receba o valor da mercadoria e forneça o valor 
+            //da entrada e das duas prestações, de acordo com as regras acima.
+            //Observe que uma justificativa para a adoção desta regra é que ela facilita a confecção e o
+            //conseqüente pagamento dos boletos das duas prestações.
+
+            //double valorMercadoria;
+            //double entrada;
+            //double prestacao;
+            //double resto;
+
+            //Console.Write("Digite o valor da mercadoria: R$ ");
+            //valorMercadoria = double.Parse(Console.ReadLine()!);
+
+            //// Calcula a entrada e as duas prestações
+            //resto = valorMercadoria % 3;
+            //prestacao = (valorMercadoria - resto) / 3;
+            //entrada = prestacao + resto;
+
+            //Console.WriteLine("Valor da entrada: " + entrada);
+            //Console.WriteLine("Valor das prestaçoes: " + prestacao);
+
+            //--------------------------------------------------------------
 
 
 
