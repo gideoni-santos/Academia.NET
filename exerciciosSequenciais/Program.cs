8265572d8e43a16ecb2d9af5e7ecf58ca98c900e
Gideoni Santos <115567537+gideoni-santos@users.noreply.github.com>
2023-10-04 00:26:34 -0300

    questão 1, 2 e 3 finalizadas

--- exerciciosSequenciais/Program.cs
+++ exerciciosSequenciais/Program.cs
@@ -4,9 +4,92 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 1) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre dois números quaisquer.
 
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Digite um número: ");
+            //int num1 = int.Parse(Console.ReadLine()!);
+
+            //Console.WriteLine("Digite um outro número: ");
+            //int num2 = int.Parse(Console.ReadLine()!);
+
+            //float media = (num1 + num2) / 2;
+            //Console.WriteLine("A média dos valores é inseridos é: " + media);
+
+            //--------------------------------------------------------------
+
+            // 2) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre quatro números quaisquer.
+
+            //Console.WriteLine("Digite um número: ");
+            //int num3 = int.Parse(Console.ReadLine()!);
+
+            //Console.WriteLine("Digite um outro número: ");
+            //int num4 = int.Parse(Console.ReadLine()!);
+
+            //Console.WriteLine("Digite um número: ");
+            //int num5 = int.Parse(Console.ReadLine()!);
+
+            //Console.WriteLine("Digite um outro número: ");
+            //int num6 = int.Parse(Console.ReadLine()!);
+
+            //double media2 = (num3 + num4 + num5 + num6) / 4.0; //Inserir o 4.0 para que o resultado saia com as casas decimais
+            //Console.WriteLine("A média dos valores é inseridos é: " + media2);
+
+            //--------------------------------------------------------------
+
+            // 3) Escreva a seqüência de atribuições, em forma de programa em C# e no Visual Studio, para trocar entre si os valores de duas variáveis A e B.
+
+            //string nome1 = "Gideoni";
+            //Console.WriteLine("O conteúdo de nome1 é: " + nome1);
+
+            //string nome2 = "Fernanda";
+            //Console.WriteLine("O conteúdo de nome2 é: " + nome2);
+
+            //string backup = nome1;
+            //Console.WriteLine("O conteúdo de backup é: " + backup);
+
+            //nome1 = nome2;
+            //Console.WriteLine("O conteúdo de nome1 sobrescreveu e agora é: " + nome1);
+
+            //nome2 = backup;
+            //Console.WriteLine("O conteúdo de nome2 sobrescreveu e agora é: " + nome2);
+
+            //// - Manipulação de string
+            //Console.Write("Digite uma frase qualquer: ");
+            //string frase = Console.ReadLine();
+
+            ////Console.WriteLine("Você digitou....: " + frase.ToUpper()); //ToUpper é usado para transormar em maiusculo
+            ////Console.WriteLine("Esta frase tem " + frase.Length + " caracteres"); //Lenght é usado para contar quantas caracteres tem em uma frase
+
+            //Console.Write("Digite algo para pesquisar: ");
+            //string palavra = Console.ReadLine();
+
+            //if (frase.Contains(palavra))
+            //{
+            //    Console.WriteLine(palavra + " aparece na frase");
+            //    //bloco de instruções caso a palavra esteja na frase
+            //}
+            //else
+            //{
+            //    Console.WriteLine(palavra + " não aparece na frase");
+            //    //bloco de instruções caso a palavra não esteja na frase
+            //}
+
+            //--------------------------------------------------------------
+
+            // 4) Faça um programa em C# e no Visual Studio que receba a data na forma DDMMAAAA e imprima na forma AAAAMMDD e AAMMDD.Note que o dia, o mês e o ano devem ser lidos em variáveis diferentes.
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
