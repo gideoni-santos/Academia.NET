e373e60ddb6e956138d4da0681d424f0fc1eeba9
Gideoni Santos <115567537+gideoni-santos@users.noreply.github.com>
2023-10-27 16:34:47 -0300

    Update Program.cs

--- exercicio_POO_Alexandre/Program.cs
+++ exercicio_POO_Alexandre/Program.cs
@@ -1,10 +1,79 @@
-﻿namespace exercicio_POO_Alexandre
+﻿using System.Collections.Generic;
+
+namespace exercicio_POO_Alexandre
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<Atleta> listaAtletas = new List<Atleta>();
+
+            do
+            {
+                Console.WriteLine("Menu Padel");
+                Console.WriteLine("1 - Cadastrar Atleta");
+                Console.WriteLine("2 - Listar Atletas");
+                Console.WriteLine("3 - Remover Atleta");
+                Console.WriteLine("7 - Sair");
+                Console.Write("Opção: ");
+                opcao = Console.ReadLine();
+
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Cadastro");
+                        Comunicacao.cadastrarAtleta(listaAtletas);
+                        //Persistencia.gravarAtleta(listaAtletas)
+
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Listagem de atletas");
+                        Comunicacao.listarAtletas(listaAtletas);
+
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Remoção de atleta");
+                        Comunicacao.removerAtletas(listaAtletas);
+                        break;
+
+                    case "7":
+                        break;
+
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            } while (true);
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
