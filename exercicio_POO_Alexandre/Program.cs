426959574741cef0d5e5f1a3cbf43eee82a6eb6f
Gideoni Santos <115567537+gideoni-santos@users.noreply.github.com>
2023-10-27 16:15:52 -0300

    exercicio de poo alexandre
    fazer correçoes

---
+++ exercicio_POO_Alexandre/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exercicio_POO_Alexandre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
